f619c12eee74a7bae9abeaf75871a8cbc693bfe1
Baanzito <diegocontatoprof@gmail.com>
2024-04-01 18:07:13 -0300

    Atualizando.

--- src/main/java/me/dio/sdw24/adapters/out/ChampionsJdbcRepository.java
+++ src/main/java/me/dio/sdw24/adapters/out/ChampionsJdbcRepository.java
@@ -1,6 +1,6 @@
 package me.dio.sdw24.adapters.out;
 
-import me.dio.sdw24.domain.model.Champions;
+import me.dio.sdw24.domain.model.Champion;
 import me.dio.sdw24.domain.ports.ChampionsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -14,11 +14,11 @@ import java.util.Optional;
 public class ChampionsJdbcRepository implements ChampionsRepository {
 
     private final JdbcTemplate jdbcTemplate;
-    private final RowMapper<Champions> rowMapper;
+    private final RowMapper<Champion> rowMapper;
 
     public ChampionsJdbcRepository(JdbcTemplate jdbcTemplate){
         this.jdbcTemplate = jdbcTemplate;
-        this.rowMapper = (rs, rowNumb) -> new Champions(
+        this.rowMapper = (rs, rowNumb) -> new Champion(
                 rs.getLong("id"),
                 rs.getString("name"),
                 rs.getString("role"),
@@ -28,14 +28,15 @@ public class ChampionsJdbcRepository implements ChampionsRepository {
     }
 
     @Override
-    public List<Champions> findAll() {
+    public List<Champion> findAll() {
+
         return jdbcTemplate.query("SELECT * FROM CHAMPIONS", rowMapper);
     }
 
     @Override
-    public Optional<Champions> findById(Long id) {
+    public Optional<Champion> findById(Long id) {
         String sql = "SELECT * FROM CHAMPIONS WHERE ID = ?";
-        Champions champions = jdbcTemplate.queryForObject(sql, rowMapper, id);
-        return Optional.ofNullable(champions);
+        List<Champion> champions = jdbcTemplate.query(sql, rowMapper, id);
+        return champions.stream().findFirst();
     }
 }
